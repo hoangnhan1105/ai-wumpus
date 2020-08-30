b68b71ba8643daa0b5693987f2159a3a9672b7c2
hoangnhan1105 <nguyenhoangnhan0@gmail.com>
2020-08-30 10:59:27 +0700

    Agent: Completed 75%
    Fix bug

--- Sources/agent.py
+++ Sources/agent.py
@@ -36,6 +36,54 @@ class agent:
         self.visited = []
         self.path = []
 
+    def convert_pos_to_index(self, pos):
+        return pos[1] * 10 + pos[0] + 1
+
+    def convert_index_to_pos(self, index):
+        return (index - 1) % 10, (index - 1) // 10
+
+    def get_adjacent_cells_pos(self, pos):
+        adj_cells_pos = []
+        for i in [-1, 1]:
+            if 0 <= pos[0] + i <= 9:
+                adj_cells_pos.append((pos[0] + i, pos[1]))
+            if 0 <= pos[1] + i <= 9:
+                adj_cells_pos.append((pos[0], pos[1] + i))
+        return adj_cells_pos
+
+    def print_clause(self, clause):
+        for i, literal in enumerate(clause):
+            if self.BREEZE_OFFSET + 1 <= abs(literal) <= self.BREEZE_OFFSET + 100:
+                if literal > 0:
+                    print('Breeze(', literal, ')', sep='', end='')
+                else:
+                    print('NOT Breeze(', -literal, ')', sep='', end='')
+            elif self.PIT_OFFSET + 1 <= abs(literal) <= self.PIT_OFFSET + 100:
+                if literal > 0:
+                    print('Pit(', literal % self.PIT_OFFSET, ')', sep='', end='')
+                else:
+                    print('NOT Pit(', -literal % self.PIT_OFFSET, ')', sep='', end='')
+            elif self.STENCH_OFFSET + 1 <= abs(literal) <= self.STENCH_OFFSET + 100:
+                if literal > 0:
+                    print('Stench(', literal % self.STENCH_OFFSET, ')', sep='', end='')
+                else:
+                    print('NOT Stench(', -literal % self.STENCH_OFFSET, ')', sep='', end='')
+            elif self.WUMPUS_OFFSET + 1 <= abs(literal) <= self.WUMPUS_OFFSET + 100:
+                if literal > 0:
+                    print('Wumpus(', literal % self.WUMPUS_OFFSET, ')', sep='', end='')
+                else:
+                    print('NOT Wumpus(', -literal % self.WUMPUS_OFFSET, ')', sep='', end='')
+            elif self.SAFE_OFFSET + 1 <= abs(literal) <= self.SAFE_OFFSET + 100:
+                if literal > 0:
+                    print('Safe(', literal % self.SAFE_OFFSET, ')', sep='', end='')
+                else:
+                    print('NOT Safe(', -literal % self.SAFE_OFFSET, ')', sep='', end='')
+
+            if i < len(clause) - 1:
+                print(' OR ', end='')
+            else:
+                print('')
+
     def init_kb(self):
         self.knowledge_base = []
 
@@ -51,11 +99,14 @@ class agent:
         # ^
         # Breeze(x, y) v ~Pit(x, y + 1)
         for i in range(1, 101):
+            cell_pos = self.convert_index_to_pos(i)
+            adj_cells_pos = self.get_adjacent_cells_pos(cell_pos)
+            adj_cells_idx = [self.convert_pos_to_index(pos) for pos in adj_cells_pos]
+
             clause_list = [[-(self.BREEZE_OFFSET + i)]]
-            for j in [-10, -1, 1, 10]:
-                if 1 <= i + j <= 100:
-                    clause_list[0].append(self.PIT_OFFSET + i + j)
-                    clause_list.append([i, -(self.PIT_OFFSET + i + j)])
+            for adj_idx in adj_cells_idx:
+                clause_list[0].append(self.PIT_OFFSET + adj_idx)
+                clause_list.append([self.BREEZE_OFFSET + i, -(self.PIT_OFFSET + adj_idx)])
             self.knowledge_base += clause_list
 
         # Stench(x, y) <=> Wumpus(x, y - 1) v Wumpus(x - 1, y) v Wumpus(x + 1, y) v Wumpus(x, y + 1)
@@ -70,11 +121,14 @@ class agent:
         # ^
         # Stench(x, y) v ~Wumpus(x, y + 1)
         for i in range(1, 101):
+            cell_pos = self.convert_index_to_pos(i)
+            adj_cells_pos = self.get_adjacent_cells_pos(cell_pos)
+            adj_cells_idx = [self.convert_pos_to_index(pos) for pos in adj_cells_pos]
+
             clause_list = [[-(self.STENCH_OFFSET + i)]]
-            for j in [-10, -1, 1, 10]:
-                if 1 <= i + j <= 100:
-                    clause_list[0].append(self.WUMPUS_OFFSET + i + j)
-                    clause_list.append([self.STENCH_OFFSET + i, -(self.WUMPUS_OFFSET + i + j)])
+            for adj_idx in adj_cells_idx:
+                clause_list[0].append(self.WUMPUS_OFFSET + adj_idx)
+                clause_list.append([self.STENCH_OFFSET + i, -(self.WUMPUS_OFFSET + adj_idx)])
             self.knowledge_base += clause_list
 
         # ~Pit(x, y) ^ ~Wumpus(x, y) <=> Safe(x, y)
@@ -93,6 +147,11 @@ class agent:
         start_idx = self.convert_pos_to_index(self.START_POS)
         self.knowledge_base.append([self.SAFE_OFFSET + start_idx])
 
+        print('Initial KB:')
+        for clause in self.knowledge_base:
+            self.print_clause(clause)
+        print('')
+
     def perceive(self, raw_map, pos):
         new_percept = percept(pos=pos)
         if 'b' in raw_map[pos[1]][pos[0]]:
@@ -100,21 +159,6 @@ class agent:
         if 's' in raw_map[pos[1]][pos[0]]:
             new_percept.stench = True
         return new_percept
-
-    def convert_pos_to_index(self, pos):
-        return pos[1] * 10 + pos[0] + 1
-
-    def convert_index_to_pos(self, index):
-        return (index - 1) % 10, (index - 1) // 10
-
-    def get_adjacent_cells_pos(self, pos):
-        adj_cells_pos = []
-        for i in [-1, 1]:
-            if 0 <= pos[0] + i <= 9:
-                adj_cells_pos.append((pos[0] + i, pos[1]))
-            if 0 <= pos[1] + i <= 9:
-                adj_cells_pos.append((pos[0], pos[1] + i))
-        return adj_cells_pos
 
     def infer_new_knowledge(self, new_percept):
         self.pos = new_percept.pos
@@ -130,7 +174,6 @@ class agent:
         else:
             self.knowledge_base.append([-(self.STENCH_OFFSET + cell_idx)])
 
-        # <WORK IN PROGRESS>
         """
         adj_cells_pos = self.get_adjacent_cells_pos(new_percept.pos)
         adj_cells_idx = [self.convert_pos_to_index(pos) for pos in adj_cells_pos]
@@ -144,17 +187,8 @@ class agent:
             sol = g.solve()
             if not sol:
                 self.knowledge_base.append([self.WUMPUS_OFFSET + adj_idx])
-        
-        # Ask if there is a SAFE cell among adjacent cells.
-        for adj_idx in adj_cells_idx:
-            g = Glucose3()
-            for clause in self.knowledge_base:
-                g.add_clause(clause)
-            g.add_clause([-(self.SAFE_OFFSET + adj_idx)])
-            sol = g.solve()
-            if not sol:
-                self.knowledge_base.append([self.SAFE_OFFSET + adj_idx])
         """
+        # Frontier cell: an UNVISITED cell which is adjacent to at least one VISITED cell.
         frontier_cells_pos = []
         for y in range(10):
             for x in range(10):
@@ -166,6 +200,7 @@ class agent:
                     frontier_cells_pos.append((x, y))
         frontier_cells_idx = [self.convert_pos_to_index(pos) for pos in frontier_cells_pos]
 
+        # For each frontier cell, ask if it is SAFE.
         for frontier_idx in frontier_cells_idx:
             if [self.SAFE_OFFSET + frontier_idx] in self.knowledge_base:
                 continue
@@ -176,11 +211,6 @@ class agent:
             sol = g.solve()
             if not sol:
                 self.knowledge_base.append([self.SAFE_OFFSET + frontier_idx])
-
-        # </WORK IN PROGRESS>
-
-    def manhattan_distance(self, pos_1, pos_2):
-        return abs(pos_1[0] - pos_2[0]) + abs(pos_1[1] - pos_2[1])
 
     def find_path(self, destination_list_pos):
         node = self.pos
